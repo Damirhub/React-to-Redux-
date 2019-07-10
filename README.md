b29711589f63fea45aba14ebae67e4ee1f5ac597
Damirhub <damir.pjetlovic@gmail.com>
2019-07-10 11:32:31 +0200

    import redux to app

--- README.md
+++
@@ -1,2 +0,0 @@
-# React-to-Redux-
-refactoring React only app to React-Redux app
--- src/index.js
+++ src/index.js
@@ -3,4 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './components/App/App';
 import './index.css';
 
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import { promiseMiddleware } from 'redux-promise'
+
 ReactDOM.render(<App />, document.getElementById('root'));
